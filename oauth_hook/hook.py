160b5035b10a7728c4d1982d5a6fff31a7ffc64a
Miguel Araujo Perez <miguel.araujo.perez@gmail.com>
2011-11-20 17:12:33 +0100

    Removing unnecessary attributes from the hook class

--- oauth_hook/hook.py
+++ oauth_hook/hook.py
@@ -29,8 +29,6 @@ class CustomSignatureMethod_HMAC_SHA1(SignatureMethod_HMAC_SHA1):
 
 class OAuthHook(object):
     OAUTH_VERSION = '1.0'
-    consumer_key = None
-    consumer_secret = None
     signature = CustomSignatureMethod_HMAC_SHA1()
 
     def __init__(self, access_token=None, access_token_secret=None, consumer_key=None, consumer_secret=None):
